29a34a0a5f1cf78930363efc1c2f7aac4d35410f
barrosAlessandro <alessandro.eletronica@gmail.com>
2020-12-03 16:19:14 -0300

    new codes while loop

---
+++ src/maiorIdadeWhileLoop.kt
@@ -0,0 +1,14 @@
+fun maiorIdade(idade: Int) {
+
+    if (idade >= 18) println("Maior de idade") else println("Menor de idade")
+
+}
+
+fun main() {
+    maiorIdade(idade = 18)
+    maiorIdade(idade = 0)
+    maiorIdade(idade = 19)
+    maiorIdade(idade = 33)
+    maiorIdade(idade = 15)
+}
+
